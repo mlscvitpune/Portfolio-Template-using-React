f32173670b1bb1b6578f5b2ab189b2958b1d6e45
Agnibha Sarkar <agnibha10@gmail.com>
2022-10-01 20:02:25 +0530

    initial commit

---
+++ src/components/Contact.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div
+      name='contact'
+      className='w-full h-screen bg-[#0a192f] flex justify-center items-center p-4'
+    >
+      <p className='text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300'>
+        Contact
+      </p>
+    </div>
+  );
+};
+
+export default Contact;
